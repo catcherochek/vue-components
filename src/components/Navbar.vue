1b8ddfe474a3112c5d89cbd5769e78b761f4ee58
cacherochek <cacherochek@yandex.ru>
2019-11-11 00:48:23 +0200

    Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo center">{{msg}}</a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li v-for="item in menublocks">
+            <a :href="item.href">{{item.name}}</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'navbar',
+
+    props: {
+      menublocks: Array,
+      msg:String,
+    },
+
+    mounted: function () {
+      //console.log(menublocks);
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
